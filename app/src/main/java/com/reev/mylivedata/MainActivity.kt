f43028f5fa25cfa1101ec9f2f74b5bb4a5a1bfef
heireev <muhammadrifqi216@gmail.com>
2023-03-13 18:13:42 +0800

    memanggil metode getElapsedTime yang ada pada MainViewModel

--- app/src/main/java/com/reev/mylivedata/MainActivity.kt
+++ app/src/main/java/com/reev/mylivedata/MainActivity.kt
@@ -2,6 +2,7 @@ package com.reev.mylivedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.reev.mylivedata.databinding.ActivityMainBinding
 
@@ -15,5 +16,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(activityMainBinding.root)
 
         liveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        subcribe()
+    }
+
+    private fun subcribe() {
+        val elapsedTimeObserver = Observer<Long?> { aLong ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, aLong)
+            activityMainBinding.timerTextview.text = newText
+        }
+
+        liveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
     }
 }
